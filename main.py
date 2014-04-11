ae72ac3eb16cc616747832ad2bf113abf56eadc8
Joshua Beitler <joshbeitler@gmail.com>
2014-04-11 08:39:33 -0600

    Moving cloud, leaves a black trace though

--- main.py
+++ main.py
@@ -2,14 +2,27 @@ import pygame,sys
 from pygame.locals import *
 import MAP
 
+class Cloud:
+    def __init__(self):
+        self.cloud = pygame.image.load("cloud.png").convert_alpha()
+        self.rect = pygame.rect.Rect(-64,50, 60, 28)
+    
+    def draw(self, surface):
+        surface.blit(self.cloud, self.rect)
+
+    def update(self):
+        self.rect.x += 1
+
+
 pygame.init()
 DISPLAYSURF = pygame.display.set_mode((MAP.MAPWIDTH * MAP.TILESIZE, MAP.MAPHEIGHT * MAP.TILESIZE))
 
 pygame.display.set_caption('Tank Game')
 DISPLAYSURF.fill((10, 218, 255))
 
+mcloud = Cloud()
+
 while True:
- 
     #get all the user events
     for event in pygame.event.get():
         #if the user wants to quit
@@ -26,5 +39,8 @@ while True:
             if MAP.tilemap[row][column] == 1:
                 pygame.draw.rect(DISPLAYSURF, (210,180,140), (column * MAP.TILESIZE, row * MAP.TILESIZE, MAP.TILESIZE, MAP.TILESIZE))
  
+    mcloud.update()
+    mcloud.draw(DISPLAYSURF)
+
     #update the display
     pygame.display.update()
